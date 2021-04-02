dea3ea84dbda18b286ac5eb095be9135f19cfccc
vincent <33580023+vincentluol@users.noreply.github.com>
2021-04-02 17:42:33 -0400

    finished A6

--- src/main/java/com/example/wbdvsp2102vluoserverjava/models/Widget.java
+++ src/main/java/com/example/wbdvsp2102vluoserverjava/models/Widget.java
@@ -1,11 +1,103 @@
 package com.example.wbdvsp2102vluoserverjava.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="widgets")
 public class Widget {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String topicIc;
+  private String name;
   private String type;
+  private int widgetOrder;
   private Integer size;
   private String text;
+  private String src;
+  private int width;
+  private int height;
+  private String cssClass;
+  private String style;
+  private String value;
+
+  public boolean isOrdered() {
+    return ordered;
+  }
+
+  public void setOrdered(boolean ordered) {
+    this.ordered = ordered;
+  }
+
+  private boolean ordered;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getWidgetOrder() {
+    return widgetOrder;
+  }
+
+  public void setWidgetOrder(int widgetOrder) {
+    this.widgetOrder = widgetOrder;
+  }
+
+  public String getSrc() {
+    return src;
+  }
+
+  public void setSrc(String src) {
+    this.src = src;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public void setWidth(int width) {
+    this.width = width;
+  }
+
+  public int getHeight() {
+    return height;
+  }
+
+  public void setHeight(int height) {
+    this.height = height;
+  }
+
+  public String getCssClass() {
+    return cssClass;
+  }
+
+  public void setCssClass(String cssClass) {
+    this.cssClass = cssClass;
+  }
+
+  public String getStyle() {
+    return style;
+  }
+
+  public void setStyle(String style) {
+    this.style = style;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
 
   public Long getId() {
     return id;
--- src/main/java/com/example/wbdvsp2102vluoserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp2102vluoserverjava/services/WidgetService.java
@@ -1,6 +1,8 @@
 package com.example.wbdvsp2102vluoserverjava.services;
 
 import com.example.wbdvsp2102vluoserverjava.models.Widget;
+import com.example.wbdvsp2102vluoserverjava.repositories.WidgetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,60 +11,43 @@ import java.util.List;
 
 @Service
 public class WidgetService {
-  private List<Widget> widgets = new ArrayList<Widget>();
-  {
-    Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Widgets for Topic ABC123");
-    Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "Lorem Ipsum");
-    Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Widgets for Topic ABC234");
-    Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
-    Widget w5 = new Widget(567l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
 
-    widgets.add(w1);
-    widgets.add(w2);
-    widgets.add(w3);
-    widgets.add(w4);
-    widgets.add(w5);
-  }
+
+  @Autowired
+  WidgetRepository repository;
 
   public Widget createWidgetForTopic(String topicId, Widget widget) {
     widget.setTopicIc(topicId);
-    widget.setId((new Date()).getTime());
-    widgets.add(widget);
-    return widget;
+//    widget.setId((new Date()).getTime());
+
+    return repository.save(widget);
   }
 
   public List<Widget> findAllWidgets() {
-    return widgets;
+    return (List<Widget>) repository.findAll();
   }
   public List<Widget> findWidgetsForTopic(String topicId) {
-    List<Widget> ws = new ArrayList<Widget>();
-    for(Widget w: widgets) {
-      if(w.getTopicIc().equals(topicId)) {
-        ws.add(w);
-      }
-    }
-    return ws;
+    return repository.findWidgetsForTopic(topicId);
   }
 
   public Integer deleteWidget(Long id) {
-    int index = -1;
-    for(int i=0; i<widgets.size(); i++) {
-      if(widgets.get(i).getId().equals(id)) {
-        index = i;
-        widgets.remove(index);
-        return 1;
-      }
-    }
-    return -1;
+    repository.deleteById(id);
+    return 1;
   }
 
   public Integer updateWidget(Long id, Widget widget) {
-    for(int i=0; i<widgets.size(); i++) {
-      if(widgets.get(i).getId().equals(id)) {
-        widgets.set(i, widget);
-        return 1;
-      }
-    }
-    return -1;
+    Widget originalWidget = repository.findById(id).get();
+    originalWidget.setText(widget.getText());
+    originalWidget.setTopicIc(widget.getTopicIc());
+    originalWidget.setType(widget.getType());
+    originalWidget.setSize(widget.getSize());
+    originalWidget.setOrdered(widget.isOrdered());
+    originalWidget.setSrc(widget.getSrc());
+    originalWidget.setHeight(widget.getHeight());
+    originalWidget.setWidth(widget.getWidth());
+
+    repository.save(originalWidget);
+
+    return 1;
   }
 }
